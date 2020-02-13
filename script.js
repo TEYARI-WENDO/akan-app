380e41fd3422f4575f977d9c6821473cb6c087b5
WE-GITHAKA <githakawendowanjiru@gmail.com>
2020-02-14 02:26:26 +0300

    Adding the validation functions

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-    
+  
+  // the validation functions
+  function dayValidator () {
+
+  }if (dayOfBirth < 1 || dayOfBirth > 31){
+        return false;
+      } else {
+        return true;
+      }
+
+
+  function monthValidator () {
+   
+    if (monthOfBirth < 1 || monthOfBirth > 12) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+ //arrays for both female and male Akan names
+ let maleAkanNames = [
+    "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Kwaku", "Yaw", "Kofi", "Kwame"
+  ];
+
+  let femaleAkanNames = [
+    "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"
+  ];
