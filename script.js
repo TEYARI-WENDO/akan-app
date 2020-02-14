e9132a314980ab826de5dd6954728155b2ceb550
WE-GITHAKA <githakawendowanjiru@gmail.com>
2020-02-14 11:19:04 +0300

    Adding functions to get gender

--- script.js
+++ script.js
@@ -26,3 +26,27 @@
   let femaleAkanNames = [
     "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"
   ];
+
+function getAkan name () {
+    let yearOfBirth =document.getElementById("year-input").value;
+    let monthOfBirth=Number(document.getElementById("month-input").value);
+    let dayOfBirth=Number(document.getElementById("day-input").value);
+    let genders =document.getElementsByName("gender")
+}
+
+//function to get gender
+
+function getGender () {
+
+    for(let gender of genders){
+
+        if(gender.checked){
+
+            return gender.value;
+
+        }
+    }
+}
+let myGenderValue=getGender();
+
+console.log(myGenderValue);
