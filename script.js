9d4396a3023337aaa7e6c6dd117b5736708eb8e9
WE-GITHAKA <githakawendowanjiru@gmail.com>
2020-02-17 10:17:34 +0300

    Changed format of the form

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 
-      
-function getAkanName () 
-{
+function getAkanName () {
   let yearOfBirth = document.getElementById("year-input").value;
   let monthOfBirth = Number(document.getElementById("month-input").value);
   let dayOfBirth = Number(document.getElementById("day-input").value);
@@ -34,24 +32,22 @@ function getAkanName ()
         return false;
       } else if (monthOfBirth === 2 && dayOfBirth > 29) {
         return false;
-      } else {
-        return true;
-      }
-    }
-  
-    function dayValidator () {
-      if (monthOfBirth === 2 && Number(yearOfBirth)%4 === 0) {
-
-      } else if (dayOfBirth < 1 || dayOfBirth > 31){
+      } else if (monthOfBirth === 2 && dayOfBirth < 1) {
         return false;
       } else {
         return true;
       }
+    } else if (dayOfBirth < 1 || dayOfBirth > 31){
+      return false;
+    } else {
+      return true;
     }
-  
-    //validation variables
-    let monthValid = monthValidator();
-    let dayValid = dayValidator();
+  }
+
+  //validation variables
+  let monthValid = monthValidator();
+  let dayValid = dayValidator();
+
   //formula to determine day of birth (Sunday = 1, Monday = 2)etc..
   let dayOfWeekNumber = Math.floor((((Number(yearOfBirth.slice(0,2))/4)-2*Number(yearOfBirth.slice(0,2))-1)+
           ((5*Number(yearOfBirth.slice(2,4))/4))+((26*(monthOfBirth+1)/10))+dayOfBirth)%7);
@@ -93,7 +89,6 @@ function getAkanName ()
     document.querySelector('h1').textContent = "Hello" + " " + femaleAkanNames[index];
     return false;
   } else {
-    alert("SORRY PLEASE FILL IN VALID INFORMATION")
+    alert("You entered an invalid day or month, please try again");
   }
-}
 }
